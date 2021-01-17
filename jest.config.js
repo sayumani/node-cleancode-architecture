8c8a0f0278708159b6ca46ed51482080bfbb4dd8
Sayooj V R <sayoojvylappully@gmail.com>
2021-01-17 16:33:15 +0530

    icov coverage report

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
-  coverageReporters: ["json", "html"],
+  coverageReporters: ["json", "html", "lcov"],
 };
